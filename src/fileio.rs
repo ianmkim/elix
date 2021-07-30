857651621d50022bdae0bb06ad45381511057bbc
Ian Kim <ian@ianmkim.com>
2021-07-30 20:03:48 +0900

    added cli

--- src/fileio.rs
+++ src/fileio.rs
@@ -6,31 +6,21 @@ use std::{
 use std::fs;
 
 /// Read a file at once
-pub fn bench_read_file_once() -> Vec<u8> {
-    let filename = &String::from("test_data/edited_v2.mp4");
+pub fn read_file_once(filename:String) -> Vec<u8> {
+    let filename = &String::from(filename);
     let mut f = File::open(&filename).expect("no file found");
     let metadata = fs::metadata(&filename).expect("unable to read metadata");
     let mut buffer = vec![0; metadata.len() as usize];
     f.read(&mut buffer).expect("buffer overflow");
-
-    /*
-    println!("Uncompressed Length: {}", buffer.len());
-    let mut inp = BufReader::new(brotli::CompressorReader::new(buffer.as_slice(), 4096, 1, 22));
-    println!("CompressorReader made");
-    let mut new_buf:Vec<u8> = Vec::new();
-    inp.read_to_end(&mut new_buf);
-    println!("Compressed Length:   {}", new_buf.len());
-    */
-
     buffer
 }
 
+
 /// Read a file by breaking it into chunks
-pub fn bench_read_file_chunk() -> Vec<u8> {
+pub fn read_file_chunk(filename:String) -> Vec<u8> {
     // chunk size approximately 1 megabyte
     const CAP:usize = 1024 * 1024;
     // test file 1.2 gigs
-    let filename = String::from("test_data/edited_v2.mp4");
     let file = File::open(&filename).unwrap();
     let mut reader = BufReader::with_capacity(CAP, file);
     // the appended final vector of bytes (represented by unsigned 8 bit integer)
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,53 @@
 use std::collections::HashMap;
 
 mod networking;
+mod fileio;
 use crate::networking::*;
+use crate::fileio::*;
+
+// cli arg parser
+use clap::{Arg, App};
 
 fn main() {
-    let res = search_for_peer();
-    println!("Received connection from main {:?}", res.unwrap().peer_addr());
+    let mut app = App::new("Elix")
+        .version("0.1.0")
+        .author("Ian Kim <ian@ianmkim.com>")
+        .about("A small, fast, and dirty file transfer utility")
+        .subcommand(App::new("send")
+            .about("Sends a file using Elix")
+            .arg(Arg::new("filename")
+                .index(1)
+                .required(true)
+                .about("A relative path to the file you want to send")))
+        .subcommand(App::new("take")
+            .about("Receive a file using Elix given a code")
+            .arg(Arg::new("code")
+                .index(1)
+                .required(true)
+                .about("A code that was either generated or given when sending a file")));
+
+    let matches = app.clone().get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("send"){
+        let filename = String::from(matches.value_of("filename").unwrap());
+        // read file from io and store as byte vector
+        let file:Vec<u8> = read_file_once(filename);
+    } else if let Some(ref matches) = matches.subcommand_matches("take"){
+        let _code = matches.value_of("code").unwrap();
+    } else {
+        app.print_long_help();
+    }
 }
 
 // benchmarking code from here on out
 mod benchmark;
-use crate::benchmark::{read_file_once, read_file_chunk};
+use crate::benchmark::{bench_read_file_once, bench_read_file_chunk};
 use std::time::Instant;
 
 fn benchmark_file_loading() {
     let mut benchmarks: HashMap<&str, &dyn Fn() -> Vec<u8>> = HashMap::new();
-    benchmarks.insert("read_file_once", &read_file_once);
-    benchmarks.insert("read_file_chunk", &read_file_chunk);
+    benchmarks.insert("read_file_once", &bench_read_file_once);
+    benchmarks.insert("read_file_chunk", &bench_read_file_chunk);
 
     let mut prev:Vec<u8> = Vec::new();
     for (i, (k, bm)) in benchmarks.into_iter().enumerate() {
