72ae418725b576c680c0e2bfc362bcebc805e4c8
parv <42292798+parvusvox@users.noreply.github.com>
2021-08-20 11:51:45 -0400

    added autodiscover fork to submodule

--- simple_client/target/debug/deps/simple_client-4dc20ea3b5c49a17.d
+++
@@ -1,5 +0,0 @@
-/home/parvusvox/rust/elix/simple_client/target/debug/deps/simple_client-4dc20ea3b5c49a17: src/main.rs
-
-/home/parvusvox/rust/elix/simple_client/target/debug/deps/simple_client-4dc20ea3b5c49a17.d: src/main.rs
-
-src/main.rs:
